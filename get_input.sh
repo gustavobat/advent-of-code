86496ab5204f2d61dff9c604704f3a12561ce46f
Gustavo Batistela <gustavoabatistela@gmail.com>
2023-08-02 16:10:50 -0300

    chore: improve get_input script

--- get_input.sh
+++ get_input.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 YEAR=$1
-DAY=$2
+# Remove leading zeros from day
+((DAY=10#$2))
 SESSION=$(cat session.txt)
+
+# If year in format YY, prefix it with '20'
+if [[ $YEAR =~ ^[0-9]{2}$ ]]; then
+  YEAR="20$YEAR"
+fi
+
+# Create data directory if it doesn't exist
+mkdir -p "$YEAR/data"
+
+# Download input
 curl -b "session=$SESSION" "https://adventofcode.com/$YEAR/day/$DAY/input" \
   > "$YEAR/data/$(printf %02d "$DAY").input"
