7776547d695c40a9c670574fb852cf6540e63dc7
Gustavo Batistela <gustavoabatistela@gmail.com>
2023-10-25 08:49:28 -0300

    fix: remove unnecessary muts

--- 2022/src/bin/10.rs
+++ 2022/src/bin/10.rs
@@ -7,20 +7,20 @@ fn get_acc_signal_strength(commands: &[String]) -> i32 {
     let mut acc_signal_strength = 0;
 
     // Clock tick callback
-    let mut on_tick = |x_reg: &mut i32| {
+    let mut on_tick = |x_reg: i32| {
         if (clock_cycles + 1) % 40 == 20 {
-            acc_signal_strength += *x_reg * (clock_cycles + 1);
+            acc_signal_strength += x_reg * (clock_cycles + 1);
         }
         clock_cycles += 1;
     };
 
     // Process commands
     commands.iter().for_each(|cmd| {
-        on_tick(&mut x_reg);
+        on_tick(x_reg);
         if cmd != "noop" {
             let (_, val_str) = cmd.split_once(' ').unwrap();
             let val = val_str.parse::<i32>().unwrap();
-            on_tick(&mut x_reg);
+            on_tick(x_reg);
             x_reg += val;
         }
     });
@@ -34,12 +34,12 @@ fn draw_display_image(commands: &[String]) -> String {
     let mut display = String::new();
 
     // Clock tick callback
-    let mut on_tick = |x_reg: &mut i32| {
+    let mut on_tick = |x_reg: i32| {
         let pos = clock_cycles % 40;
         if pos == 0 && clock_cycles != 0 {
             display += "\n";
         }
-        let sprite_pos: [i32; 3] = [*x_reg - 1, *x_reg, *x_reg + 1];
+        let sprite_pos: [i32; 3] = [x_reg - 1, x_reg, x_reg + 1];
         if sprite_pos.contains(&pos) {
             display += "#";
         } else {
@@ -50,11 +50,11 @@ fn draw_display_image(commands: &[String]) -> String {
 
     // Process commands
     commands.iter().for_each(|cmd| {
-        on_tick(&mut x_reg);
+        on_tick(x_reg);
         if cmd != "noop" {
             let (_, val_str) = cmd.split_once(' ').unwrap();
             let val = val_str.parse::<i32>().unwrap();
-            on_tick(&mut x_reg);
+            on_tick(x_reg);
             x_reg += val;
         }
     });
